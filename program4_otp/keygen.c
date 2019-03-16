6319793b0550eebae0ccb68c7f219b606f2a92e6
Phi Luu <philectron@users.noreply.github.com>
2019-03-15 20:18:27 -0700

    Implement keygen

--- program4_otp/keygen.c
+++ program4_otp/keygen.c
@@ -21,11 +21,52 @@
 // Outputs:
 //   All output text is output to  stdout , if any.
 //   All error text is output to  stderr , if any.
+//
+// Note: 27 allowed characters are 26 capital letters and the space character.
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main(void) {
-    printf("Hello world!\n");
+#define RAND_RANGE     (char)27  // 26 capital alphabet letters + 1 space
+#define SPACE_RAND_IDX (char)(RAND_RANGE - 1)  // 0-25 for letters, 26 for space
+
+int main(int argc, char** argv) {
+    // ensure correct usage of command line arguments
+    if (argc != 2) {
+        fprintf(stderr, "Usage: ./keygen keylength\n");
+        return 1;
+    }
+
+    // try convert 1st arg from string to int
+    // atoi()  returns 0 on non-numeric string, but in this case  keylength
+    // must be positive. So, if the return value is not positive, exit
+    int keylength = atoi(argv[1]);
+    if (keylength <= 0) {
+        fprintf(stderr, "Usage: ./keygen keylength\n");
+        fprintf(stderr, " keylength  must be a positive integer\n");
+        return 1;
+    }
+
+    // seed the random number generator
+    srand(time(0));
+
+    // create a random string character by character
+    for (int i = 0; i < keylength; i++) {
+        // randomize an integer between [0, RAND_RANGE - 1]
+        char rand_idx = rand() % RAND_RANGE;
+
+        // if randomized to index used for space, output space
+        if (rand_idx == SPACE_RAND_IDX) {
+            printf(" ");
+        } else {
+            // otherwise (randomized to a capital letter), output that letter
+            printf("%c", 'A' + rand_idx);
+        }
+    }
+
+    // end the key with a newline
+    printf("\n");
 
     return 0;
 }
